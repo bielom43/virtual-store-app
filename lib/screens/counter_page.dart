a342be163a531c11b110c27429c89e11cfd43719
bielom43 <bielom43@gmail.com>
2023-03-21 20:04:12 -0300

    #007 test: testing implementation for inheritedWidgets with a comum counter

---
+++ lib/screens/counter_page.dart
@@ -0,0 +1,47 @@
+//package
+import 'package:clothing_store/providers/counter.dart';
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({
+    super.key,
+  });
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exemplo Contador'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Text(CounterProvider.of(context)?.state.value.toString() ?? '0'),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                CounterProvider.of(context)?.state.inc();
+              });
+              print(CounterProvider.of(context)?.state.value.toString());
+            },
+            icon: Icon(Icons.add),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                CounterProvider.of(context)?.state.dec();
+              });
+              print(CounterProvider.of(context)?.state.value.toString());
+            },
+            icon: Icon(Icons.remove),
+          ),
+        ],
+      ),
+    );
+  }
+}
