067724641718e877f80771bbfa40795102ad8a3f
bielom43 <bielom43@gmail.com>
2023-03-30 15:27:12 -0300

    #010 add: created navigations and funcionalitie in maindrawer

--- lib/components/main_drawer.dart
+++ lib/components/main_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:clothing_store/utils/routes/app_routes.dart';
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -6,7 +7,31 @@ class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Container(),
+      child: Column(
+        children: [
+          AppBar(
+            title: const Text('Bem Vindo!'),
+            automaticallyImplyLeading: false,
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.shop),
+            title: const Text('Loja'),
+            onTap: () => Navigator.of(context).pushReplacementNamed(
+              AppRoutes.home,
+            ),
+          ),
+          const Divider(),
+          ListTile(
+            leading: const Icon(Icons.payment),
+            title: const Text('Pedidos'),
+            onTap: () => Navigator.of(context).pushReplacementNamed(
+              AppRoutes.ORDERS,
+            ),
+          ),
+          const Divider(),
+        ],
+      ),
     );
   }
 }
