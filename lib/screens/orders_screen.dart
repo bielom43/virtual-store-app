f8fbf43129e1aed5833f895e37bb679c092fb682
bielom43 <bielom43@gmail.com>
2024-01-04 06:36:55 -0300

    using an alternative widget in orders screen to work in async cases (FutureBuilder)

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,26 +6,8 @@ import 'package:clothing_store/models/order_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   const OrdersScreen({super.key});
-
-  @override
-  State<OrdersScreen> createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() => _isLoading = false);
-    });
-  }
 
   Future<void> _refreshProducts(BuildContext context) {
     return Provider.of<OrderList>(
@@ -36,25 +18,45 @@ class _OrdersScreenState extends State<OrdersScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meus Pedidos'),
       ),
       body: RefreshIndicator(
         onRefresh: () => _refreshProducts(context),
-        child: _isLoading
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : ListView.builder(
-                itemCount: orders.itemsCount,
-                itemBuilder: (context, index) => OrderWidget(
-                  order: orders.items[index],
-                ),
-              ),
+        child: FutureBuilder(
+          future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+          builder: (ctx, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.error != null) {
+              return Center(
+                child: Text('Ocorreu um erro! Contate o Administrador do sistema.'),
+              );
+            } else {
+              return Consumer<OrderList>(
+                builder: (context, orders, child) => ListView.builder(
+                    itemCount: orders.itemsCount,
+                    itemBuilder: (context, index) => OrderWidget(order: orders.items[index])),
+              );
+            }
+          },
+        ),
       ),
+
+      // body: RefreshIndicator(
+      //   onRefresh: () => _refreshProducts(context),
+      //   child: _isLoading
+      //       ? Center(
+      //           child: CircularProgressIndicator(),
+      //         )
+      //       : ListView.builder(
+      //           itemCount: orders.itemsCount,
+      //           itemBuilder: (context, index) => OrderWidget(
+      //             order: orders.items[index],
+      //           ),
+      //         ),
+
       drawer: const MainDrawer(),
     );
   }
