9cf9cd957cfeab1495baab4d8ddbe109926c7bb5
bielom43 <bielom43@gmail.com>
2024-02-07 20:58:15 -0300

    fix: bug in logout, fixed removing local data when logout funcion is called.

--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -114,8 +114,7 @@ class Auth with ChangeNotifier {
     _expiryDate = null;
 
     _clearLogoutTimer();
-
-    notifyListeners();
+    Store.remove('userData').then((_) => notifyListeners());
   }
 
   void _autoLogout() {
